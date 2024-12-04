780f00f2a8de548622a04b42bd00fcd7f05f77ec
SaidurIUT <saidurrahman@iut-dhaka.edu>
2024-12-04 14:32:43 +0600

    Project done

---
+++ chat-forntend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatRoom from "./component/ChatRoom";
+
+const App = () => {
+  return (
+    <div>
+      <ChatRoom />
+    </div>
+  );
+};
+
+export default App;
